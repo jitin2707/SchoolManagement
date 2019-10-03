2088627c087cefce3ce742f3fa1dfbd08648adce
jitin2707 <truepx247@gmail.com>
2019-10-03 23:23:07 +0530

    added signup model

--- myUser/admin.py
+++ myUser/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from myUser.models import UserRole
+from myUser.models import UserRole,UserSignup
 
 admin.site.register(UserRole)
+admin.site.register(UserSignup)
