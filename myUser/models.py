3af7789b96042c15fe71c3d815e23f06d42b385d
jitin2707 <truepx247@gmail.com>
2019-10-03 10:03:34 +0530

    added user role and folders

--- myUser/models.py
+++ myUser/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class UserRole(models.Model):
+    roleId=models.AutoField(primary_key=True)
+    roleName=models.CharField(max_length=200,default="")
+    isActive=models.BooleanField(default=True)
+
