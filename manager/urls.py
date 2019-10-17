ce5e7f3ff2e9f7fa9578ea1f6362810874799355
jitin2707 <truepx247@gmail.com>
2019-10-17 10:54:37 +0530

    principal page work

--- manager/urls.py
+++ manager/urls.py
@@ -1,9 +1,11 @@
 from manager import views
 from django.conf.urls import url
+from django.conf.urls.static import static
+from django.conf import settings
 
 app_name = 'manager'
 urlpatterns = [
     url(r'^passwordchange/$',views.passwordchange,name="passch"),
     url(r'^home/$',views.manager),
     url(r'^profile/$',views.managerprofile,name="mprofile"),
-]
+] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- manager/views.py
+++ manager/views.py
@@ -5,6 +5,8 @@ from django.core.files.storage import FileSystemStorage
 from django.contrib.auth.hashers import make_password,check_password
 from authorize import authcheck
 from myUser.models import UserSignup
+from manager.forms import PrincipleForm
+from myUser.forms import UserSignupForm
 
 def passwordchange(request):
     if request.method=="POST":
@@ -49,4 +51,55 @@ def managerprofile(request):
     data = UserSignup.objects.get(userEmail = emailid)
     return render(request,"profile.html",{'d':data})
 
+
+def createprincipal(request):
+    if request.method == "POST":
+        form =PrincipleForm(request.POST)
+        f = form.save(commit=False)
+        try:
+            if request.FILES["img1"]:
+            my_file = request.FILES["img1"]
+            fs = FileSystemStorage()
+            file_name = fs.save(my_file.name, my_file)
+            img1 = fs.url(file_name)
+            img1 = my_file.name
+        except:
+            pass
+
+        f.name = request.POST["pn"]
+        f.email = request.POST["pe"]
+        f.gender = make_password(request.POST["pg"])
+        f.qualification = request.POST["t3"]
+        f.password = request.POST["pp"]
+        f.dob = request.POST["pd"]
+        f.doj = request.POST["t3"]
+        f.address = request.POST["t3"]
+        f.mobile = request.POST["t3"]
+        f.status = request.POST["t3"]
+        f.session = request.POST["t3"]
+        f.image = request.POST["t3"]
+        f.last_login_time = request.POST["t3"]
+        f.last_login_date = request.POST["t3"]
+        f.last_logout = request.POST["t3"]
+        f.otp = request.POST["t3"]
+        f.otp_date_time = request.POST["t3"]
+        f.verify_link = request.POST["t3"]
+        f.role_id = 2
+        f.save()
+        form1 = UserSignupForm(request.POST)
+        f = form1.save(commit=False)
+        f.userFullName= request.POST["t3"]
+        f.userEmail = request.POST["t3"]
+        f.userPassword = request.POST["t3"]
+        f.userMobile = request.POST["t3"]
+        f.userState = request.POST["t3"]
+        f.userOTP = request.POST["t3"]
+        f.otpTime = request.POST["t3"]
+        f.userToken = request.POST["t3"]
+        f.isVerified = request.POST["t3"]
+        f.confirmationLink = request.POST["t3"]
+        f.isActive = request.POST["t3"]
+        f.roleId_id = 1
+        return render(request, "signup3.html", {'success': True})
+    return render(request, "signup3.html")
 
