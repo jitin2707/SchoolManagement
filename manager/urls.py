eaa47a582a71f97eca14696fda8e6fae960bb7d0
ankita-rajput <050ankita@gmail.com>
2019-10-21 15:19:32 +0530

    principal  and teacher || forms,views,urls

--- manager/urls.py
+++ manager/urls.py
@@ -5,10 +5,10 @@ from django.conf import settings
 
 app_name = 'manager'
 urlpatterns = [
-    url(r'^passwordchange/$',views.passwordchange,name="passch"),
-    url(r'^home/$',views.manager),
-    url(r'^profile/$',views.managerprofile,name="mprofile"),
-    url(r'^addprincipal/$',views.createprincipal,name="addprinci"),
+    url(r'^passwordchange/$', views.passwordchange, name="passch"),
+    url(r'^home/$', views.manager),
+    url(r'^profile/$', views.managerprofile, name="mprofile"),
+    url(r'^addprincipal/$', views.createprincipal, name="addprinci"),
     url(r'^viewprincipal/$', views.viewprincipal, name="viewprincipal"),
-    url(r'^createteacher/$',views.createteacher,name="createteacher")
-] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+    url(r'^createteacher/$', views.createteacher, name="createteacher")
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
