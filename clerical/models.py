cc0bc74592e37f5aea7750132bb9d9434467f343
jitin2707 <truepx247@gmail.com>
2019-11-04 08:59:15 +0530

    db update

--- clerical/models.py
+++ clerical/models.py
@@ -1,3 +1,22 @@
 from django.db import models
-
+from myUser.models import UserRole
 # Create your models here.
+class Clerk(models.Model):
+    name = models.CharField(max_length=255, default="", null=True)
+    email = models.EmailField(max_length=255, default="", primary_key=True)
+    gender = models.CharField(max_length=255, default="", null=True)
+    qualification = models.CharField(max_length=255, default="", null=True)
+    password = models.CharField(max_length=255, default="", null=True)
+    dob = models.CharField(max_length=255, default="", null=True)
+    doj = models.CharField(max_length=255, default="", null=True)
+    dol = models.CharField(max_length=255, default="", null=True)
+    address = models.CharField(max_length=255, default="", null=True)
+    mobile = models.CharField(max_length=255, default="", null=True)
+    status = models.NullBooleanField(default=True)
+    session = models.CharField(max_length=255, default="", null=True)
+    image = models.CharField(max_length=255, default="", null=True)
+    last_login_time = models.CharField(max_length=255, default="", null=True)
+    last_login_date = models.CharField(max_length=255, default="", null=True)
+    last_logout = models.CharField(max_length=255, default="", null=True)
+    role = models.ForeignKey(UserRole, on_delete=models.CASCADE)
+
