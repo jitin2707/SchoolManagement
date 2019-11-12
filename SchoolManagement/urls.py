5369efcdd91ac807525f4b0e1cf8e944c317ec04
ankita-rajput <050ankita@gmail.com>
2019-11-12 11:56:31 +0530

    clerical

--- SchoolManagement/urls.py
+++ SchoolManagement/urls.py
@@ -33,6 +33,7 @@ urlpatterns = [
     url(r'^principal/',include('principal.urls')),
     url(r'^teacher/', include('teacher.urls')),
     url(r'^clerk/', include('clerical.urls')),
+    url(r'^student/', include('students.urls')),
     url(r'^error404/$',views.error404),
     url(r'^logout/$',views.logout),
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
